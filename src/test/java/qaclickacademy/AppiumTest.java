ba122a6aebdc15c2eadb0d6404f7b2a299ac3b9b
Pranav <hanwantepranav@gmail.com>
2020-03-09 17:06:45 +0530

    my first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppANdroid() {
+		System.out.println("NativeAppANdroid");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+	
+
+}
